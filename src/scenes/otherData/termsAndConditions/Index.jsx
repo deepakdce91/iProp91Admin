07ffc05951ee36d70be6678898a240bbd1a9c2c5
adityazyzz <tuskersadi@gmail.com>
2025-05-06 10:23:07 +0530

    added 3 pages

---
+++ src/scenes/otherData/termsAndConditions/Index.jsx
@@ -0,0 +1,312 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useState, useEffect } from "react";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { tokens } from "../../../theme";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import axios from "axios";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Header from "../../../components/Header";
+import { jwtDecode } from "jwt-decode";
+import { formatDate } from "../../../MyFunctions";
+import TermsAndConditionsForm from "../../../components/frontendPreview/TermsAndConditionsForm";
+
+function Index() {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const [userId, setUserId] = useState("");
+  const [userToken, setUserToken] = useState("");
+
+  const [mode, setMode] = useState("display");
+  const [data, setData] = useState([]);
+
+  const [editData, setEditData] = useState();
+
+  const columns = [
+    {
+      field: "serial",
+      headerName: "No.",
+      width: 70,
+      valueGetter: (params) => params.api.getRowIndex(params.id) + 1, // Start numbering from 1
+    },
+    {
+      field: "text",
+      headerName: "Content Preview",
+      flex: 2,
+      renderCell: (params) => {
+        // Extract text content without HTML tags for preview
+        const textPreview = params.value.replace(/<[^>]*>/g, '').substring(0, 100) + '...';
+        return <div>{textPreview}</div>;
+      }
+    },
+    {
+      field: "createdAt",
+      headerName: "Created at",
+      flex: 1,
+      valueGetter: (params) => formatDate(params.value),
+    },
+    {
+      field: "updatedAt",
+      headerName: "Last Updated",
+      flex: 1,
+      valueGetter: (params) => formatDate(params.value),
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      flex: 1,
+      renderCell: (params) => (
+        <Box>
+          <IconButton
+            onClick={() => handleEdit(params.row._id)}
+            className="text-grey-400"
+          >
+            <EditIcon />
+          </IconButton>
+          <IconButton
+            onClick={() => handleDelete(params.row._id)}
+            color="secondary"
+          >
+            <DeleteIcon />
+          </IconButton>
+        </Box>
+      ),
+    },
+  ];
+
+  const setModeToDisplay = () => {
+    setMode("display");
+    fetchAllPrivacyPolicies(userId, userToken);
+  };
+
+  const fetchPrivacyPolicy = async (id) => {
+    await axios
+      .get(
+        `${process.env.REACT_APP_BACKEND_URL}/api/termsAndConditions/fetch/${id}?userId=${userId}`,
+        {
+          headers: {
+            "auth-token": userToken,
+          },
+        }
+      )
+      .then((response) => {
+        if (response) {
+          setEditData(response.data);
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        toast.error("Some ERROR occurred.");
+      });
+  };
+
+  const fetchAllPrivacyPolicies = (userId, userToken) => {
+    axios
+      .get(
+        `${process.env.REACT_APP_BACKEND_URL}/api/termsAndConditions/fetchAll?userId=${userId}`,
+        {
+          headers: {
+            "auth-token": userToken,
+          },
+        }
+      )
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  const deletePrivacyPolicyById = async (id) => {
+    await axios
+      .delete(
+        `${process.env.REACT_APP_BACKEND_URL}/api/termsAndConditions/delete/${id}?userId=${userId}`,
+        {
+          headers: {
+            "auth-token": userToken,
+          },
+        }
+      )
+      .then((response) => {
+        if (response) {
+          fetchAllPrivacyPolicies(userId, userToken);
+          toast.success("T&C deleted!");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        toast.error("Some ERROR occurred.");
+      });
+  };
+
+  useEffect(() => {
+    try {
+      // getting userId and userToken
+      let token = localStorage.getItem("iProp-token");
+      if (token) {
+        const decoded = jwtDecode(token);
+        setUserId(decoded.userId);
+        setUserToken(token);
+        fetchAllPrivacyPolicies(decoded.userId, token);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const handleAddMore = () => {
+    setMode("add");
+  };
+
+  const handleCancel = () => {
+    setMode("display");
+    fetchAllPrivacyPolicies(userId, userToken);
+  };
+
+  // Click handler for the edit button
+  const handleEdit = (id) => {
+    fetchPrivacyPolicy(id);
+
+    setTimeout(() => {
+      setMode("edit");
+    }, 500);
+  };
+
+  // Click handler for the delete button
+  const handleDelete = (id) => {
+    deletePrivacyPolicyById(id);
+  };
+
+  return (
+    <Box m="20px">
+      {/* HEADER */}
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header
+          title="Terms and Conditions Management"
+          subtitle={
+            mode === "add"
+              ? "Add Terms and Conditions"
+              : mode === "edit"
+              ? "Edit Terms and Conditions"
+              : "Manage Terms and Conditions here"
+          }
+        />
+
+        <Box>
+          {mode === "display" ? (
+            <div
+              className="border-2 mr-12 border-blue-600 rounded-lg px-3 py-2 text-blue-400 cursor-pointer hover:bg-blue-600 hover:text-blue-200"
+              onClick={handleAddMore}
+            >
+              Add
+            </div>
+          ) : (
+            <div
+              className="border-2 mr-12 border-red-600 rounded-lg px-3 py-2 text-red-400 cursor-pointer hover:bg-red-600 hover:text-red-200"
+              onClick={handleCancel}
+            >
+              Back
+            </div>
+          )}
+        </Box>
+      </Box>
+
+      {/* Render form or DataGrid based on mode */}
+      {mode === "add" ? (
+        <TermsAndConditionsForm
+          setModeToDisplay={setModeToDisplay}
+          userId={userId}
+          userToken={userToken}
+        />
+      ) : mode === "edit" ? (
+        editData && (
+          <Box
+            m="40px 0 0 0"
+            height="75vh"
+            sx={{
+              "& .MuiDataGrid-root": {
+                border: "none",
+              },
+              "& .MuiDataGrid-cell": {
+                borderBottom: "none",
+              },
+              "& .name-column--cell": {
+                color: colors.greenAccent[300],
+              },
+              "& .MuiDataGrid-columnHeaders": {
+                backgroundColor: colors.blueAccent[700],
+                borderBottom: "none",
+              },
+              "& .MuiDataGrid-virtualScroller": {
+                backgroundColor: colors.primary[400],
+              },
+              "& .MuiDataGrid-footerContainer": {
+                borderTop: "none",
+                backgroundColor: colors.blueAccent[700],
+              },
+              "& .MuiCheckbox-root": {
+                color: `${colors.greenAccent[200]} !important`,
+              },
+              "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+                color: `${colors.grey[100]} !important`,
+              },
+            }}
+          >
+            <TermsAndConditionsForm
+              editData={editData}
+              setModeToDisplay={setModeToDisplay}
+              userId={userId}
+              userToken={userToken}
+            />
+          </Box>
+        )
+      ) : (
+        <Box
+          m="40px 0 0 0"
+          height="75vh"
+          sx={{
+            "& .MuiDataGrid-root": {
+              border: "none",
+            },
+            "& .MuiDataGrid-cell": {
+              borderBottom: "none",
+            },
+            "& .name-column--cell": {
+              color: colors.greenAccent[300],
+            },
+            "& .MuiDataGrid-columnHeaders": {
+              backgroundColor: colors.blueAccent[700],
+              borderBottom: "none",
+            },
+            "& .MuiDataGrid-virtualScroller": {
+              backgroundColor: colors.primary[400],
+            },
+            "& .MuiDataGrid-footerContainer": {
+              borderTop: "none",
+              backgroundColor: colors.blueAccent[700],
+            },
+            "& .MuiCheckbox-root": {
+              color: `${colors.greenAccent[200]} !important`,
+            },
+            "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+              color: `${colors.grey[100]} !important`,
+            },
+          }}
+        >
+          <DataGrid
+            rows={data}
+            columns={columns}
+            components={{ Toolbar: GridToolbar }}
+            getRowId={(row) => row._id}
+            autoHeight
+          />
+        </Box>
+      )}
+    </Box>
+  );
+}
+
+export default Index;
